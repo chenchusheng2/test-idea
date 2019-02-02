f66fd610645416e826d0d6af4d44ed4050a8ad60
root <root>
2019-02-02 16:37:38 +0800

    第一次提交到本地

---
+++ src/main/java/com/itheima/web/test01.java
@@ -0,0 +1,12 @@
+package com.itheima.web;
+
+/**
+ * @创建人 GMchen
+ * @创建时间 ${DATA}
+ * 描述
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("测试类执行了 ");
+    }
+}
